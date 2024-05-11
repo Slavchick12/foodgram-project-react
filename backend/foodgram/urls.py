d8272e8df3534c8a9cd5d3fb8dee8552220540d5
slava <v.turukin@ocm-expert.com>
2024-05-11 15:15:12 +0300

    fix: static and app name

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -11,7 +11,4 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
